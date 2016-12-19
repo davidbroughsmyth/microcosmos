d711aab65512a8d588a9d389bbc04a753dd3228c
Maurício Szabo <mauricio.szabo@gmail.com>
2016-12-19 18:54:51 -0200

    Subscribe on queues and default components.

---
+++ src/components/core.clj
@@ -0,0 +1,52 @@
+(ns components.core
+  (:require [components.future :as future]
+            [components.logging :as log]))
+
+(defprotocol IO
+  (listen [component function]
+          "Listens to new connections. Expects a function that will be called
+with a single parameter - a Map containing (at least) :payload, which is the
+message sent to this service")
+  (send! [component message]
+        "Sends a new message to this component. Different services can need
+different keys. For instance, RabbitMQ needs :payload only but accepts :meta,
+HTTP probably will need :payload, :header and :response.")
+  (ack! [component param]
+       "Sends an acknowledge that our last message was processed. Some services
+may need this, some not. In queue services, informs the server that we processed
+this last message. In some HTTP services, it can indicate (in case of streaming)
+that the connection should be ended and we sent all data we had to send.
+Some services can choose to ignore this.")
+  (reject! [component param ex]
+          "Rejects this message. Indicates to service thar some error has
+occurred, and some action should be done. Some services can choose to ignore
+this"))
+
+(defn generate-cid [old-cid]
+  (let [upcase-chars (map char (range (int \A) (inc (int \Z))))
+        digits (range 10)
+        alfa-digits (cycle (map str (concat upcase-chars digits)))
+        cid-gen #(apply str (take % (random-sample 0.02 alfa-digits)))]
+    (if old-cid
+      (str old-cid "." (cid-gen 5))
+      (cid-gen 8))))
+
+(defn decorate-logger [logger cid]
+  (let [new-cid (generate-cid cid)]
+    (reify log/Log
+      (log [_ message type data]
+           (log/log logger message type (assoc data :cid new-cid))))))
+
+(defn subscribe-with [ & {:as components}]
+  (let [components (update components :logger #(or % log/default-logger))]
+    (fn [component callback]
+      (listen component (fn [data]
+                          (let [cid (get-in data [:meta :cid])
+                                components (update components :logger decorate-logger cid)]
+                            (->> (callback (future/just data) components)
+                                 (future/on-success (fn [_] (ack! component data)))
+                                 (future/on-failure (fn [ex]
+                                                      (log/fatal (:logger components)
+                                                                 "Uncaught Exception"
+                                                                 :ex ex)
+                                                      (reject! component data ex))))))))))
---
+++ test/components/core_test.clj
@@ -0,0 +1,51 @@
+(ns components.core-test
+  (:require [midje.sweet :refer :all]
+            [components.core :as components]
+            [components.future :as future]
+            [components.logging :as log]))
+
+(defn fake-component [other]
+  (let [fn (atom nil)]
+    (reify components/IO
+      (send! [_ msg] (@fn msg))
+      (listen [_ f] (reset! fn f))
+      (ack! [_ msg] (components/send! other {:ack msg}))
+      (reject! [_ msg ex] (components/send! other {:reject msg})))))
+
+(def log-output (atom nil))
+(def subscribe (components/subscribe-with
+                 :logger (reify log/Log
+                           (log [_ msg type data]
+                                (reset! log-output {:msg msg :type type :data data})))))
+
+(facts "when subscribing for new messages"
+  (let [last-msg (atom nil)
+        other (fake-component nil)
+        component (fake-component other)]
+    (components/listen other (fn [msg] (reset! last-msg msg)))
+
+    (fact "sends an ACK when messages were processed"
+      (subscribe component (fn [a _] a))
+      (components/send! component "some-msg")
+      @last-msg => {:ack "some-msg"})
+
+    (fact "sends a REJECT when messages fail"
+      (subscribe component (fn [f _] (future/map #(Integer/parseInt %) f)))
+      (components/send! component "ten")
+      @last-msg => {:reject "ten"})
+
+    (fact "logs CID when using default logger"
+      (do
+        (subscribe component (fn [data {:keys [logger]}]
+                               (log/info logger "Foo")
+                               data))
+        (components/send! component "some-msg")
+        @log-output) => {:msg "Foo", :type :info, :data {:cid "FOOBAR"}}
+      (provided
+        (components/generate-cid nil) => "FOOBAR"))
+
+    (fact "logs an error using logger and CID to correlate things"
+      (subscribe component (fn [f _] (future/map #(Integer/parseInt %) f)))
+      (components/send! component "ten")
+      @log-output => (contains {:type :fatal, :data (contains {:cid string?
+                                                               :ex anything})}))))
