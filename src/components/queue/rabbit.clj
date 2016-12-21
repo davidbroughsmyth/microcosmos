c40e409af14513f70d32e95fbf2656558f696c8a
Maurício Szabo <mauricio.szabo@gmail.com>
2016-12-21 19:53:46 -0200

    Metadata parsing from Rabbit

--- src/components/queue/rabbit.clj
+++ src/components/queue/rabbit.clj
@@ -1,34 +1,51 @@
 (ns components.queue.rabbit
   (:require [cheshire.core :as json]
+            [cheshire.generate :as generators]
+            [clojure.core :as clj]
             [components.core :as components]
             [langohr.basic :as basic]
             [langohr.channel :as channel]
             [langohr.consumers :as consumers]
             [langohr.core :as core]
             [langohr.exchange :as exchange]
-            [langohr.queue :as queue]))
+            [langohr.queue :as queue])
+  (:import [com.rabbitmq.client.impl LongStringHelper$ByteArrayLongString]))
+
+(generators/add-encoder LongStringHelper$ByteArrayLongString generators/encode-str)
+
+(defn- parse-payload [payload]
+  (-> payload (String. "UTF-8") (json/decode true)))
+
+(defn- parse-meta [meta]
+  (let [headers (->> meta
+                     :headers
+                     (map (fn [[k v]] [(keyword k) v]))
+                     (into {}))]
+    (-> headers (merge meta) (dissoc :headers))))
 
 (defrecord Queue [channel name max-tries]
   components/IO
   (listen [_ function]
     (consumers/subscribe channel name
                          (fn [_ meta payload]
-                           (function {:payload (-> payload (String. "UTF-8") (json/decode true))
-                                      :meta meta}))))
+                           (function {:payload (parse-payload payload)
+                                      :meta (parse-meta meta)}))))
 
-  (send! [_ {:keys [payload meta]}]
-    (basic/publish channel "" name
-                   (json/encode payload)
-                   meta)))
+  (send! [_ {:keys [payload meta] :or {meta {}}}]
+    (let [headers (->> meta
+                       (map (fn [[k v]] [(clj/name k) v]))
+                       (into {}))]
+      headers (apply dissoc headers (mapv str (keys meta)))
+      (basic/publish channel "" name
+                     (json/encode payload)
+                     (assoc meta :headers headers))))
 
-  ; (ack! [_]))
+  (ack! [_ {:keys [meta]}]
+    (basic/ack channel (:delivery-tag meta)))
 
-  ; (listen [_ function]
-  ;   (let [f (fn [_ meta payload] (function {:payload (-> payload
-  ;                                                       (String. "UTF-8")
-  ;                                                       edn/read-string)
-  ;                                           :meta meta}))]
-  ;     (consumers/subscribe channel name f {:auto-ack false}))))
+  (reject! [_ {:keys [meta]} ex]
+    (basic/reject channel (:delivery-tag meta))))
+
 
 (def connection (atom nil))
 (def channel (atom nil))
--- test/components/queue/rabbit_test.clj
+++ test/components/queue/rabbit_test.clj
@@ -10,28 +10,42 @@
   (future/map (fn [value]
                 (case value
                   "error" (throw (java.lang.InternalError. "Some Error"))
-                  (components/send! (queue "test-result") {:payload "foo"})))
+                  (components/send! (queue "test-result") value)))
               fut-value))
 
 (defn result-for-message [msg]
-  (let [sub (components/subscribe-with :queue #(rabbit/queue % :auto-delete true))
+  (let [sub (components/subscribe-with :queue #(rabbit/queue % :auto-delete true)
+                                       :logger (components.logging/->DebugLogger))
         test-queue (rabbit/queue "test" :auto-delete true :max-retries 1)
         result-queue (rabbit/queue "test-result" :auto-delete true)
         p (promise)]
     (sub test-queue send-msg)
     (sub result-queue (fn [future _]
                         (future/map (fn [value]
-                                      (deliver p value)))))
-    (components/send! test-queue {:payload msg})
+                                      (println "RES" value)
+                                      (deliver p value))
+                                    future)))
+    (components/send! test-queue msg)
     p))
 
-(defn as-str [promise]
-  (json/parse-string (deref promise 1000 "null") true))
-
-(background
-   (before :facts (rabbit/connect!))
-   (after :facts (rabbit/disconnect!)))
+(defn send-and-wait [msg]
+  (deref (result-for-message msg) 1000 {:payload :timeout
+                                        :meta :timeout}))
 
 (facts "Handling messages on RabbitMQ's queue"
-   (fact "handles message if successful"
-     (as-str (result-for-message {:some "msg"}))) => {:some "msg"})
+  (fact "handles message if successful"
+    (:payload (send-and-wait {:payload {:some "msg"}})) => {:some "msg"})
+
+  (fact "attaches metadata into msg"
+    (:meta (send-and-wait {:payload {:some "msg"}, :meta {:a 10}}))
+    => (contains {:a 10}))
+
+  (fact "attaches CID between services"
+    (get-in (send-and-wait {:payload "msg"}) [:meta :cid]) => "FOO.BAR"
+    (provided
+      (components/generate-cid nil) => "FOO"
+      (components/generate-cid "FOO") => "FOO.BAR"))
+
+  (against-background
+    (before :facts (rabbit/connect!))
+    (after :facts (rabbit/disconnect!))))
