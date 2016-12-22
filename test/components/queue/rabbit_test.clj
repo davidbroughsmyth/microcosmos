157c1f6aace8955b63149c0b9369628c460109f8
Maurício Szabo <mauricio.szabo@gmail.com>
2016-12-22 15:18:11 -0200

    Tests for deadletter starting

--- test/components/queue/rabbit_test.clj
+++ test/components/queue/rabbit_test.clj
@@ -7,30 +7,45 @@
             [langohr.core :as core]
             [midje.sweet :refer :all]))
 
+(def all-msgs (atom []))
+(def all-processed (atom []))
+(def all-deadletters (atom []))
+
 (defn- send-msg [fut-value {:keys [result-q]}]
   (future/map (fn [value]
-                (case value
-                  "error" (throw (java.lang.InternalError. "Some Error"))
+                (println "VAL: " value)
+                (swap! all-msgs conj value)
+                (case (:payload value)
+                  "error" (throw (Exception. "Some Error"))
                   (components/send! result-q value)))
               fut-value))
 
-(defn result-for-message [msg]
-  (let [sub (components/subscribe-with :result-q (rabbit/queue "test-result" :auto-delete true)
-                                       :logger (components.logging/->DebugLogger))
-        test-queue (rabbit/queue "test" :auto-delete true :max-retries 1)
+(def sub (components/subscribe-with :result-q (rabbit/queue "test-result" :auto-delete true)
+                                    :logger (components.logging/->DebugLogger)))
+
+(defn in-future [f]
+  (fn [future _]
+    (future/map f future)))
+
+(defn result-for-messages [ & msgs]
+  (let [test-queue (rabbit/queue "test" :auto-delete true :max-retries 1)
         result-queue (rabbit/queue "test-result" :auto-delete true)
+        deadletter-queue (rabbit/->Queue (:channel test-queue) "test-deadletter" 1000 "FOO")
         p (promise)]
     (sub test-queue send-msg)
-    (sub result-queue (fn [future _]
-                        (future/map (fn [value]
-                                      (deliver p value))
-                                    future)))
-    (components/send! (cid/append-cid test-queue "FOO") msg)
+    (sub result-queue (in-future #(do (swap! all-processed conj %) (deliver p %))))
+    (sub deadletter-queue (in-future #(swap! all-deadletters conj %)))
+    (doseq [msg msgs]
+      (components/send! (cid/append-cid test-queue "FOO") msg))
     p))
 
 (defn send-and-wait [msg]
-  (deref (result-for-message msg) 1000 {:payload :timeout
-                                        :meta :timeout}))
+  (deref (result-for-messages msg) 1000 {:payload :timeout
+                                         :meta :timeout}))
+
+(defn wait [promise]
+  (deref promise 1000 {:payload :timeout
+                       :meta :timeout}))
 
 (facts "Handling messages on RabbitMQ's queue"
   (fact "handles message if successful"
@@ -48,4 +63,16 @@
     (components/generate-cid "FOO") => "FOO.BAR"
     (components/generate-cid "FOO.BAR") => ..irrelevant..
     (before :facts (rabbit/connect!))
+    (after :facts (rabbit/disconnect!))))
+
+(fact "when message results in a failure"
+  (fact "process message two times before generating a deadletter"
+    (:payload (wait (result-for-messages {:payload "error"} {:payload "msg"}))) => "msg"
+    (map :payload @all-processed) => ["msg"])
+
+  (against-background
+    (before :facts (do
+                     (rabbit/connect!)
+                     (reset! all-msgs [])
+                     (reset! all-deadletters [])))
     (after :facts (rabbit/disconnect!))))
