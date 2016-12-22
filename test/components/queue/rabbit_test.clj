5b6890f249541d65dc02859486d722d75b578aa9
Maurício Szabo <mauricio.szabo@gmail.com>
2016-12-22 15:51:31 -0200

    Async code testing...

--- test/components/queue/rabbit_test.clj
+++ test/components/queue/rabbit_test.clj
@@ -10,10 +10,10 @@
 (def all-msgs (atom []))
 (def all-processed (atom []))
 (def all-deadletters (atom []))
+(def last-promise (atom (promise)))
 
 (defn- send-msg [fut-value {:keys [result-q]}]
   (future/map (fn [value]
-                (println "VAL: " value)
                 (swap! all-msgs conj value)
                 (case (:payload value)
                   "error" (throw (Exception. "Some Error"))
@@ -27,25 +27,31 @@
   (fn [future _]
     (future/map f future)))
 
-(defn result-for-messages [ & msgs]
+(defn send-messages [msgs]
   (let [test-queue (rabbit/queue "test" :auto-delete true :max-retries 1)
         result-queue (rabbit/queue "test-result" :auto-delete true)
-        deadletter-queue (rabbit/->Queue (:channel test-queue) "test-deadletter" 1000 "FOO")
-        p (promise)]
+        deadletter-queue (rabbit/->Queue (:channel test-queue) "test-deadletter" 1000 "FOO")]
     (sub test-queue send-msg)
-    (sub result-queue (in-future #(do (swap! all-processed conj %) (deliver p %))))
+    (sub result-queue (in-future #(do
+                                    (swap! all-processed conj %)
+                                    (when (realized? @last-promise)
+                                      (reset! last-promise (promise)))
+                                    (deliver @last-promise %))))
     (sub deadletter-queue (in-future #(swap! all-deadletters conj %)))
     (doseq [msg msgs]
-      (components/send! (cid/append-cid test-queue "FOO") msg))
-    p))
+      (components/send! (cid/append-cid test-queue "FOO") msg))))
 
-(defn send-and-wait [msg]
-  (deref (result-for-messages msg) 1000 {:payload :timeout
-                                         :meta :timeout}))
+(defn send-and-wait [ & msgs]
+  (send-messages msgs)
+  (deref @last-promise 1000 {:payload :timeout
+                             :meta :timeout}))
 
-(defn wait [promise]
-  (deref promise 1000 {:payload :timeout
-                       :meta :timeout}))
+(defn prepare-tests []
+  (rabbit/connect!)
+  (reset! last-promise (promise))
+  (reset! all-msgs [])
+  (reset! all-processed [])
+  (reset! all-deadletters []))
 
 (facts "Handling messages on RabbitMQ's queue"
   (fact "handles message if successful"
@@ -62,17 +68,18 @@
   (against-background
     (components/generate-cid "FOO") => "FOO.BAR"
     (components/generate-cid "FOO.BAR") => ..irrelevant..
-    (before :facts (rabbit/connect!))
+    (before :facts (prepare-tests))
     (after :facts (rabbit/disconnect!))))
 
+; OH MY GOSH, how difficult is to test asynchronous code!
 (fact "when message results in a failure"
   (fact "process message two times before generating a deadletter"
-    (:payload (wait (result-for-messages {:payload "error"} {:payload "msg"}))) => "msg"
-    (map :payload @all-processed) => ["msg"])
+    (:payload (send-and-wait {:payload "error"} {:payload "msg"})) => "msg"
+    (reset! last-promise (promise))
+    (:payload (send-and-wait {:payload "other-msg"})) => "other-msg"
+    (map :payload @all-deadletters) => ["error"]
+    (map :payload @all-msgs) => ["error" "msg" "error" "other-msg"])
 
   (against-background
-    (before :facts (do
-                     (rabbit/connect!)
-                     (reset! all-msgs [])
-                     (reset! all-deadletters [])))
-    (after :facts (rabbit/disconnect!))))
+   (before :facts (prepare-tests))
+   (after :facts (rabbit/disconnect!))))
