381b653004b1424bed411d042f5b198d07f16f23
Maurício Szabo <mauricio.szabo@gmail.com>
2016-12-22 11:53:04 -0200

    CID and LongString working

--- src/components/queue/rabbit.clj
+++ src/components/queue/rabbit.clj
@@ -2,6 +2,7 @@
   (:require [cheshire.core :as json]
             [cheshire.generate :as generators]
             [clojure.core :as clj]
+            [components.cid :as cid]
             [components.core :as components]
             [langohr.basic :as basic]
             [langohr.channel :as channel]
@@ -9,54 +10,71 @@
             [langohr.core :as core]
             [langohr.exchange :as exchange]
             [langohr.queue :as queue])
-  (:import [com.rabbitmq.client.impl LongStringHelper$ByteArrayLongString]))
+  (:import [com.rabbitmq.client LongString]))
 
-(generators/add-encoder LongStringHelper$ByteArrayLongString generators/encode-str)
+(generators/add-encoder LongString generators/encode-str)
 
 (defn- parse-payload [payload]
   (-> payload (String. "UTF-8") (json/decode true)))
 
+(def rabbit-default-meta [:cluster-id :app-id :message-id :expiration :type :user-id
+                          :delivery-tag :delivery-mode :priority :redelivery?
+                          :routing-key :content-type :persistent? :reply-to
+                          :content-encoding :correlation-id :exchange :timestamp])
+
 (defn- parse-meta [meta]
-  (let [headers (->> meta
+  (let [normalize-kv (fn [[k v]] [(keyword k) (if (instance? LongString v)
+                                                (str v)
+                                                v)])
+        headers (->> meta
                      :headers
-                     (map (fn [[k v]] [(keyword k) v]))
+                     (map normalize-kv)
                      (into {}))]
     (-> headers (merge meta) (dissoc :headers))))
 
-(defrecord Queue [channel name max-tries]
+(defn- normalize-headers [meta]
+  (let [headers (->> meta
+                     (map (fn [[k v]] [(clj/name k) v]))
+                     (into {}))]
+    (apply dissoc headers rabbit-default-meta)))
+
+(defrecord Queue [channel name max-tries cid]
   components/IO
   (listen [_ function]
-    (consumers/subscribe channel name
-                         (fn [_ meta payload]
-                           (function {:payload (parse-payload payload)
-                                      :meta (parse-meta meta)}))))
+    (let [callback (fn [_ meta payload]
+                     (function {:payload (parse-payload payload)
+                                :meta (parse-meta meta)}))]
+      (consumers/subscribe channel name callback)))
 
   (send! [_ {:keys [payload meta] :or {meta {}}}]
-    (let [headers (->> meta
-                       (map (fn [[k v]] [(clj/name k) v]))
-                       (into {}))]
-      headers (apply dissoc headers (mapv str (keys meta)))
+    (if cid
       (basic/publish channel "" name
                      (json/encode payload)
-                     (assoc meta :headers headers))))
+                     (assoc meta :headers (normalize-headers (assoc meta :cid cid))))
+      (throw (IllegalArgumentException.
+               (str "Can't publish to queue without CID. Maybe you tried to send a message "
+                    "using `queue` from components' namespace. Prefer to use the"
+                    "components' attribute to create one.")))))
 
   (ack! [_ {:keys [meta]}]
-    (basic/ack channel (:delivery-tag meta)))
+        (basic/ack channel (:delivery-tag meta)))
 
   (reject! [_ {:keys [meta]} ex]
-    (basic/reject channel (:delivery-tag meta))))
+    (basic/reject channel (:delivery-tag meta)))
+
+  cid/CID
+    (append-cid [rabbit cid]
+      (->Queue channel name max-tries cid)))
 
 
 (def connection (atom nil))
 (def channel (atom nil))
 
 (defn connect! []
-  (println "CONNECTING")
   (reset! connection (core/connect))
   (reset! channel (channel/open @connection)))
 
 (defn disconnect! []
-  (println "DISCONNECTING")
   (when @connection
     (core/close @channel)
     (core/close @connection))
@@ -84,4 +102,4 @@
 
     (exchange/fanout @channel dead-letter-name {:durable true})
     (queue/bind @channel dead-letter-q-name dead-letter-name)
-    (->Queue @channel name (:max-tries opts))))
+    (->Queue @channel name (:max-tries opts) nil)))
--- test/components/queue/rabbit_test.clj
+++ test/components/queue/rabbit_test.clj
@@ -1,20 +1,21 @@
 (ns components.queue.rabbit-test
   (:require [components.core :as components]
+            [components.cid :as cid]
             [components.future :as future]
             [components.queue.rabbit :as rabbit]
             [cheshire.core :as json]
             [langohr.core :as core]
             [midje.sweet :refer :all]))
 
-(defn- send-msg [fut-value {:keys [queue]}]
+(defn- send-msg [fut-value {:keys [result-q]}]
   (future/map (fn [value]
                 (case value
                   "error" (throw (java.lang.InternalError. "Some Error"))
-                  (components/send! (queue "test-result") value)))
+                  (components/send! result-q value)))
               fut-value))
 
 (defn result-for-message [msg]
-  (let [sub (components/subscribe-with :queue #(rabbit/queue % :auto-delete true)
+  (let [sub (components/subscribe-with :result-q (rabbit/queue "test-result" :auto-delete true)
                                        :logger (components.logging/->DebugLogger))
         test-queue (rabbit/queue "test" :auto-delete true :max-retries 1)
         result-queue (rabbit/queue "test-result" :auto-delete true)
@@ -22,10 +23,9 @@
     (sub test-queue send-msg)
     (sub result-queue (fn [future _]
                         (future/map (fn [value]
-                                      (println "RES" value)
                                       (deliver p value))
                                     future)))
-    (components/send! test-queue msg)
+    (components/send! (cid/append-cid test-queue "FOO") msg)
     p))
 
 (defn send-and-wait [msg]
@@ -38,14 +38,14 @@
 
   (fact "attaches metadata into msg"
     (:meta (send-and-wait {:payload {:some "msg"}, :meta {:a 10}}))
-    => (contains {:a 10}))
+    => (contains {:a 10, :cid "FOO.BAR"}))
 
   (fact "attaches CID between services"
     (get-in (send-and-wait {:payload "msg"}) [:meta :cid]) => "FOO.BAR"
-    (provided
-      (components/generate-cid nil) => "FOO"
-      (components/generate-cid "FOO") => "FOO.BAR"))
+    (provided))
 
   (against-background
+    (components/generate-cid "FOO") => "FOO.BAR"
+    (components/generate-cid "FOO.BAR") => ..irrelevant..
     (before :facts (rabbit/connect!))
     (after :facts (rabbit/disconnect!))))
