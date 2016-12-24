ce85a330c0301c32479acf067e3a4cb2cbc5bb23
Maurício Szabo <mauricio.szabo@gmail.com>
2016-12-24 12:37:34 -0200

    Added simple example (high load tests).

---
+++ examples/strange_sum.clj
@@ -0,0 +1,40 @@
+(ns strange-sum
+  (:require [components.core :as components]
+            [components.future :as future]
+            [components.cid :as cid]
+            [components.queue.rabbit :as rabbit]))
+
+
+(def sub (components/subscribe-with :result-q (rabbit/queue "test-result" :auto-delete true)
+                                    :logger (components.logging/->DebugLogger)))
+
+
+(defn normalize-payload [message]
+  (let [payload (:payload message)
+        n1 (:n1 payload)
+        n2 (:n2 payload)]
+    [n1 n2]))
+
+(defn sum [[n1 n2]]
+  (if (= n1 0)
+    n2
+    (recur [(dec n1) (inc n2)])))
+
+(defn publish-result [result result-q]
+  (components/send! result-q {:payload result}))
+
+(defn process-sum [future {:keys [result-q]}]
+  (->> future
+      (future/map normalize-payload)
+      (future/flat-map #(future/execute (sum %)))
+      (future/map #(publish-result % result-q))))
+
+(sub (rabbit/queue "sum") process-sum)
+
+#_(
+   (doseq [_ (range 100)]
+     (components/send! (cid/append-cid (rabbit/queue "sum") "FOOCID")
+                       {:payload {:n1 10001000 :n2 20}})))
+
+(sub (rabbit/queue "test-result" :auto-delete true)
+     (fn [f _] (future/map #(println "RES:" (:payload %)) f)))
