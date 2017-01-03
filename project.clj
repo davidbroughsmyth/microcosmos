5b0e05913512678eaba35e5c3630ab0fe32229d9
Maurício Szabo <mauricio.szabo@gmail.com>
2017-01-03 18:45:45 -0200

    Added environ.

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [com.novemberain/langohr "3.6.1"]
                  [cheshire "5.6.3"]
-                 [finagle-clojure/core "0.7.0"]]
+                 [finagle-clojure/core "0.7.0"]
+                 [environ "1.1.0"]]
   :profiles {:dev {:src-paths ["dev"]
                    :dependencies [[midje "1.8.3"]]
                    :plugins [[lein-midje "3.2.1"]]}})
