84728dd1598db3f94eec9c0d302870eef7e329e0
Maurício Szabo <mauricio.szabo@gmail.com>
2017-01-03 19:17:00 -0200

    Documentation

--- src/components/core.clj
+++ src/components/core.clj
@@ -37,25 +37,52 @@ this"))
         new-cid (generate-cid cid)]
     {:cid new-cid}))
 
-(defn subscribe-with [ & {:as components-generators}]
-  (let [components-generators (update components-generators :logger #(or % log/default-logger-gen))]
+(defn- handler-for-component [components-generators io-component callback data]
+  (let [params (params-for-generators data)
+        components (->> components-generators
+                        (map (fn [[k generator]] [k (generator params)]))
+                        (into {}))
+        logger (:logger components)
+        ack-msg (fn [_] (ack! io-component data))
+        reject-msg (fn [ex]
+                     (log/fatal logger "Uncaught Exception" :ex ex)
+                     (reject! io-component data ex))]
+    (log/info logger "Processing message" :msg data)
+    (->> (callback (future/just data) components)
+         (future/on-success ack-msg)
+         (future/on-failure reject-msg))))
+
+(defn subscribe-with
+  "Defines a subscribe function for an IO component (like a Rabbit Queue, HTTP handler,
+or something like that). Returns a function that can be used (and re-used) to subscribe
+to components
+
+Accepts key-value pairs to define additional components. A component generator function
+is, normally, a function that accepts some parameters and returns another function
+(the generator). Generators MUST accept a single parameter that'll configure
+additional data, such as `:cid` and `:mocked`.
+
+For example, to subscribe to a RabbitMQ's queue, one can use:
+
+(def subscribe (subscribe-with :result-q (queue \"result\")))
+(subscribe (queue \"data\") (fn [f-message components] .....))
+
+The callback function (that will be passed to subscribe) will be called with two
+arguments: one is the message (it will be a `Future`, and when resolved will be a map
+containing at least `:payload` and `:meta`) and other is a map containing all the
+components previously defined (in the above case, it'll be a map with only `:result-q`
+key).
+
+When subscribing to events with this function, the message being processed will be
+logged (automatically) and it'll be automatically ACKed or REJECTed in case of success
+or failure"
+  [ & {:as components-generators}]
+  (let [components-generators (update components-generators
+                                      :logger #(or % log/default-logger-gen))]
     (fn [io-gen callback]
       (let [component (io-gen (params-for-generators {}))]
-        (listen component (fn [data]
-                            (let [params (params-for-generators data)
-                                  components (->> components-generators
-                                                  (map (fn [[k generator]] [k (generator params)]))
-                                                  (into {}))]
-                              (log/info (:logger components)
-                                        "Processing message"
-                                        :msg data)
-                              (->> (callback (future/just data) components)
-                                   (future/on-success (fn [_] (ack! component data)))
-                                   (future/on-failure (fn [ex]
-                                                        (log/fatal (:logger components)
-                                                                   "Uncaught Exception"
-                                                                   :ex ex)
-                                                        (reject! component data ex)))))))))))
+        (listen component
+                (partial handler-for-component components-generators component callback))))))
 
 (defmacro mocked [ & args]
   `(let [function# ~params-for-generators]
