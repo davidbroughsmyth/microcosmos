a179b8026b08140e478fad79f440a8ada7884442
Maurício Szabo <mauricio.szabo@gmail.com>
2017-01-05 19:56:51 -0200

    Refactoring

--- src/components/crypt.clj
+++ src/components/crypt.clj
@@ -28,22 +28,6 @@
     (.writeObject writer keys)
     (.flush writer)))
 
-(defn- encrypt-or-decrypt [bytes algorithm key mode]
-  (let [cipher (doto (Cipher/getInstance algorithm "BC")
-                     (.init mode key))]
-        ; block-size (.getBlockSize cipher)]
-    ; (doseq [slice (partition-all block-size block-size bytes)]
-    ;   (println "STEP" (.getBlockSize cipher))
-    ;   (.update cipher (byte-array slice)))
-    (def c cipher)
-    (def b bytes)
-    (.doFinal cipher bytes)))
-
-(def k (gen-keys "RSA" 2048))
-
-(def text (str (range 100000)))
-(def bytess (.getBytes text "UTF-8"))
-
 (defn- concat-byte-arrays [byte-arrays]
   (let [total-size (reduce + (map count byte-arrays))
         result     (byte-array total-size)
@@ -51,39 +35,27 @@
     (doseq [ba byte-arrays] (.put bb ba))
     result))
 
-(defn asymmetric-enc [txt key]
-  (let [bytes (.getBytes txt "UTF-8")
-        public-key (PublicKeyFactory/createKey (.getEncoded key))
-        engine (doto (PKCS1Encoding. (RSAEngine.))
-                     (.init true public-key))
+(defn- encrypt-or-decrypt [bytes key encrypt?]
+  (let [engine (doto (PKCS1Encoding. (RSAEngine.))
+                     (.init encrypt? key))
         block-size (.getInputBlockSize engine)
         msg-size (count bytes)
-        enc #(conj %1 (.processBlock engine bytes %2 %3))]
+        fun #(cond-> %1 (pos? %3) (conj (.processBlock engine bytes %2 %3)))]
 
     (loop [processed 0
            cipher-text []]
       (if (> (+ processed block-size) msg-size)
-        (Base64/encodeBase64String
-          (concat-byte-arrays (enc cipher-text processed (- msg-size processed))))
+        (concat-byte-arrays (fun cipher-text processed (- msg-size processed)))
         (recur
           (+ processed block-size)
-          (enc cipher-text processed block-size))))))
+          (fun cipher-text processed block-size))))))
+
+(defn asymmetric-enc [txt key]
+  (let [bytes (.getBytes txt "UTF-8")
+        public-key (PublicKeyFactory/createKey (.getEncoded key))]
+    (Base64/encodeBase64String (encrypt-or-decrypt bytes public-key true))))
 
 (defn asymmetric-dec [txt key]
   (let [bytes (Base64/decodeBase64 txt)
-        private-key (PrivateKeyFactory/createKey (.getEncoded key))
-        engine (doto (PKCS1Encoding. (RSAEngine.))
-                     (.init false private-key))
-        block-size (.getInputBlockSize engine)
-        msg-size (count bytes)
-        dec #(cond-> %1 (pos? %3) (conj (.processBlock engine bytes %2 %3)))]
-
-    (loop [processed 0
-           cipher-text []]
-      (if (> (+ processed block-size) msg-size)
-        (String. (concat-byte-arrays
-                   (dec cipher-text processed (- msg-size processed)))
-                 "UTF-8")
-        (recur
-          (+ processed block-size)
-          (dec cipher-text processed block-size))))))
+        private-key (PrivateKeyFactory/createKey (.getEncoded key))]
+    (String. (encrypt-or-decrypt bytes private-key false) "UTF-8")))
