82d8423285e9dc37a48f699f56da8cd5b36688b8
Maurício Szabo <mauricio.szabo@gmail.com>
2017-01-06 15:10:23 -0200

    Added RSA/AES encryption

--- src/components/crypt.clj
+++ src/components/crypt.clj
@@ -1,5 +1,5 @@
 (ns components.crypt
-  (:import [java.security Security KeyPairGenerator AlgorithmParameters]
+  (:import [java.security Security KeyPairGenerator]
            [javax.crypto Cipher KeyGenerator]
            [javax.crypto.spec SecretKeySpec IvParameterSpec]
            [org.bouncycastle.jce.provider BouncyCastleProvider]
@@ -30,32 +30,32 @@
       .generateKey
       .getEncoded))
 
+(defn string->bytes [txt]
+  (if (string? txt) (.getBytes txt "UTF-8") txt))
+
+(defn base64->bytes [txt]
+  (if (string? txt) (Base64/decodeBase64 txt) txt))
+
+(defn to-base64 [txt]
+  (if (string? txt) txt (Base64/encodeBase64String txt)))
+
+(defn to-string [txt]
+  (if (string? txt) txt (String. txt "UTF-8")))
+
 (defn aes-enc [txt key]
   (let [cipher (doto (Cipher/getInstance "AES/CBC/PKCS5Padding" "BC")
                      (.init Cipher/ENCRYPT_MODE (SecretKeySpec. key "AES")))
-        bytes (.getBytes txt "UTF-8")
+        bytes (string->bytes txt)
         cipher-text (.doFinal cipher bytes)
         iv (.getIV cipher)]
-    (Base64/encodeBase64String
-     (concat-byte-arrays [iv cipher-text]))))
+    (concat-byte-arrays [iv cipher-text])))
 
 (defn aes-dec [txt key]
-  (let [bytes (Base64/decodeBase64 txt)
+  (let [bytes (base64->bytes txt)
         [iv cipher-text] (split-at 16 bytes)
         cipher (doto (Cipher/getInstance "AES/CBC/PKCS5Padding" "BC")
                      (.init Cipher/DECRYPT_MODE (SecretKeySpec. key "AES") (IvParameterSpec. (byte-array iv))))]
-    (String. (.doFinal cipher (byte-array cipher-text)) "UTF-8")))
-
-(defn slurp-key [f]
-  (-> f
-      java.io.FileReader.
-      org.bouncycastle.openssl.PEMReader.
-      .readObject))
-
-(defn spit-key [f keys]
-  (let [writer (org.bouncycastle.openssl.PEMWriter. (java.io.FileWriter. f))]
-    (.writeObject writer keys)
-    (.flush writer)))
+    (.doFinal cipher (byte-array cipher-text))))
 
 (defn- encrypt-or-decrypt [bytes key encrypt?]
   (let [engine (doto (PKCS1Encoding. (RSAEngine.))
@@ -72,12 +72,33 @@
           (+ processed block-size)
           (fun cipher-text processed block-size))))))
 
-(defn asymmetric-enc [txt key]
-  (let [bytes (.getBytes txt "UTF-8")
+(defn rsa-enc [txt key]
+  (let [bytes (string->bytes txt)
         public-key (PublicKeyFactory/createKey (.getEncoded key))]
-    (Base64/encodeBase64String (encrypt-or-decrypt bytes public-key true))))
+    (encrypt-or-decrypt bytes public-key true)))
 
-(defn asymmetric-dec [txt key]
-  (let [bytes (Base64/decodeBase64 txt)
+(defn rsa-dec [txt key]
+  (let [bytes (base64->bytes txt)
         private-key (PrivateKeyFactory/createKey (.getEncoded key))]
-    (String. (encrypt-or-decrypt bytes private-key false) "UTF-8")))
+    (encrypt-or-decrypt bytes private-key false)))
+
+(defn asymmetric-enc [txt public-key]
+  (let [key (gen-aes-key)]
+    (to-base64 (concat-byte-arrays [(rsa-enc key public-key)
+                                    (aes-enc txt key)]))))
+
+(defn asymmetric-dec [txt private-key]
+  (let [[encr-key cipher-text] (->> txt base64->bytes (split-at 256) (map byte-array))
+        key (rsa-dec encr-key private-key)]
+    (to-string (aes-dec cipher-text key))))
+
+(defn slurp-key [f]
+  (-> f
+      java.io.FileReader.
+      org.bouncycastle.openssl.PEMReader.
+      .readObject))
+
+(defn spit-key [f keys]
+  (let [writer (org.bouncycastle.openssl.PEMWriter. (java.io.FileWriter. f))]
+    (.writeObject writer keys)
+    (.flush writer)))
--- test/components/crypt_test.clj
+++ test/components/crypt_test.clj
@@ -2,16 +2,30 @@
   (:require [midje.sweet :refer :all]
             [components.crypt :as crypt]))
 
-(facts "about asymmetric encryption"
+(facts "about rsa encryption"
   (fact "encrypts a single text"
-    (let [key-pair (crypt/gen-keys "RSA" 1024)
-          encrypted (crypt/asymmetric-enc "foobar" (:public key-pair))]
+    (let [key-pair (crypt/gen-keys 2048)
+          encrypted (crypt/rsa-enc "foobar" (:public key-pair))]
       encrypted =not=> "foobar"
-      (crypt/asymmetric-dec encrypted (:private key-pair)) => "foobar"))
+      (crypt/to-string (crypt/rsa-dec encrypted (:private key-pair))) => "foobar"))
 
   (fact "encrypts a long text"
-    (let [key-pair (crypt/gen-keys "RSA" 1024)
+    (let [key-pair (crypt/gen-keys 2048)
           algo "RSA/NONE/PKCS1Padding"
           txt (str (range 800))
-          encrypted (crypt/asymmetric-enc txt (:public key-pair))]
-      (crypt/asymmetric-dec encrypted (:private key-pair)) => txt)))
+          encrypted (crypt/rsa-enc txt (:public key-pair))]
+      (crypt/to-string (crypt/rsa-dec encrypted (:private key-pair))) => txt)))
+
+(facts "about AES encryption"
+  (let [key (crypt/gen-aes-key)
+        txt (str (range 800))]
+    (fact "encrypts a long text"
+      (crypt/aes-enc txt key) =not=> txt
+      (crypt/to-string (crypt/aes-dec (crypt/aes-enc txt key) key)) => txt)))
+
+(facts "about asymmetric encription with RSA and AES"
+  (let [key (crypt/gen-keys 2048)
+        txt (str (range 800))]
+    (fact "encrypts a long text"
+      (crypt/asymmetric-enc txt (:public key)) =not=> txt
+      (crypt/asymmetric-dec (crypt/asymmetric-enc txt (:public key)) (:private key)) => txt)))
