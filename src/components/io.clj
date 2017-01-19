45cb7233d50ac336eddcd0964be0f3b4402cf600
Maurício Szabo <mauricio.szabo@gmail.com>
2017-01-19 17:05:50 -0200

    Separated IO from core

---
+++ src/components/io.clj
@@ -0,0 +1,22 @@
+(ns components.io)
+
+(defprotocol IO
+  (listen [component function]
+          "Listens to new connections. Expects a function that will be called
+with a single parameter - a Map containing (at least) :payload, which is the
+message sent to this service")
+  (send! [component message]
+        "Sends a new message to this component. Different services can need
+different keys, but at least must support :payload and :meta. For instance, RabbitMQ
+needs :payload only but accepts :meta, HTTP probably can probably accept :payload, :meta,
+and :header")
+  (ack! [component param]
+       "Sends an acknowledge that our last message was processed. Some services
+may need this, some not. In queue services, informs the server that we processed
+this last message. In some HTTP services, it can indicate (in case of streaming)
+that the connection should be ended and we sent all data we had to send.
+Some services can choose to ignore this.")
+  (reject! [component param ex]
+          "Rejects this message. Indicates to service thar some error has
+occurred, and some action should be done. Some services can choose to ignore
+this"))
