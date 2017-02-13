b49131882f5807de3d0d182bc1969eded32c2236
Maurício Szabo <mauricio.szabo@gmail.com>
2017-02-13 17:14:28 -0200

    Renamed send! to IO.

--- core/test/microscope/core_test.clj
+++ core/test/microscope/core_test.clj
@@ -14,8 +14,8 @@
     (reify io/IO
       (send! [_ msg] (@fn msg))
       (listen [_ f] (reset! fn f))
-      (ack! [_ msg] (components/send! other {:ack msg}))
-      (reject! [_ msg ex] (components/send! other {:reject msg})))))
+      (ack! [_ msg] (io/send! other {:ack msg}))
+      (reject! [_ msg ex] (io/send! other {:reject msg})))))
 
 (def log-output (atom nil))
 (defn logger [{:keys [cid]}]
@@ -32,16 +32,16 @@
         subscribe (components/subscribe-with :logger logger
                                              :queue component-gen)]
 
-    (components/listen other (fn [msg] (reset! last-msg msg)))
+    (io/listen other (fn [msg] (reset! last-msg msg)))
 
     (fact "sends an ACK when messages were processed"
       (subscribe :queue (fn [a _] a))
-      (components/send! component "some-msg")
+      (io/send! component "some-msg")
       @last-msg => {:ack "some-msg"})
 
     (fact "sends a REJECT when messages fail"
       (subscribe :queue (fn [f _] (future/map #(Integer/parseInt %) f)))
-      (components/send! component "ten")
+      (io/send! component "ten")
       @last-msg => {:reject "ten"})
 
     (fact "logs CID when using default logger"
@@ -49,21 +49,21 @@
         (subscribe :queue (fn [data {:keys [logger]}]
                             (log/info logger "Foo")
                             data))
-        (components/send! component "some-msg")
+        (io/send! component "some-msg")
         @log-output) => {:msg "Foo", :type :info, :data {:cid "FOOBAR"}}
       (provided
         (components/generate-cid nil) => "FOOBAR"))
 
     (fact "logs when processing a message"
       (subscribe :queue (fn [a _] a))
-      (components/send! component "some-msg")
+      (io/send! component "some-msg")
       @log-output => (contains {:msg "Processing message",
                                 :type :info,
                                 :data (contains {:msg "some-msg"})}))
 
     (fact "logs an error using logger and CID to correlate things"
       (subscribe :queue (fn [f _] (future/map #(Integer/parseInt %) f)))
-      (components/send! component "ten")
+      (io/send! component "ten")
       @log-output => (contains {:type :fatal, :data (contains {:cid string?
                                                                :ex anything})}))))
 
@@ -109,7 +109,7 @@
     (components/mocked
      (a-function)
      (with-out-str
-       (components/send! @queue {:payload "Some msg"})) => #"ERROR: Message\n\n\{:cid"))
+       (io/send! @queue {:payload "Some msg"})) => #"ERROR: Message\n\n\{:cid"))
 
   (fact "allows to pass parameters to mocked env"
     (components/mocked {:initial-state-val :some-initial-state}
