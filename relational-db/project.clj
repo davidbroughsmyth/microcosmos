25e9c59a44e595cd490a8c43b4e4f3263060a671
Maurício Szabo <mauricio.szabo@gmail.com>
2017-02-13 16:25:46 -0200

    Change version

--- relational-db/project.clj
+++ relational-db/project.clj
@@ -1,4 +1,4 @@
-(defproject microscope/relational-db "0.1.0-SNAPSHOT"
+(defproject microscope/relational-db "0.1.0"
   :description "Microservice architecture for Clojure"
   :url "https://github.com/acessocard/microscope"
   :license {:name "Eclipse Public License"
