b42864dca2f4b485bb6d600b805a635955a2d0b2
Maurício Szabo <mauricio.szabo@gmail.com>
2017-02-14 19:17:30 -0200

    Adding healthcheck on SQLite.

--- relational-db/test/microscope/relational_db/sqlite_test.clj
+++ relational-db/test/microscope/relational_db/sqlite_test.clj
@@ -1,6 +1,8 @@
 (ns microscope.relational-db.sqlite-test
   (:require [midje.sweet :refer :all]
+            [microscope.relational-db :as db]
             [microscope.relational-db.sqlite :as sqlite]
+            [microscope.healthcheck :as health]
             [clojure.java.jdbc :as jdbc]
             [clojure.java.io :as io]))
 
@@ -17,3 +19,21 @@
   (background
     (after :facts (do
                     (io/delete-file "__test__.db" true)))))
+
+(facts "about healthcheck"
+  (let [db (db/sqlite-memory nil)]
+    (fact "healthchecks when DB is connected"
+      (health/check {:db db}) => {:result true :details {:db nil}})
+
+    (fact "fails with connection error when DB is disconnected"
+      (.close (:datasource db))
+      (health/check {:db db}) => {:result false
+                                  :details {:db {:connection "failed simple select"}}})
+
+    (fact "fails with exception message when something REALLY STRANGE occurred"
+      (health/check {:db db}) => {:result false
+                                  :details {:db {:connection "unknown error"
+                                                 :exception-type "clojure.lang.ExceptionInfo"
+                                                 :exception-msg "strange error"}}}
+      (provided
+       (jdbc/query irrelevant irrelevant) =throws=> (ex-info "strange error" {})))))
