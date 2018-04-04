b30891105b3f8f7d05a757f3d11f74c83bce52a3
Maurício Szabo <mauricio.szabo@gmail.com>
2018-04-04 19:54:51 -0300

    Migrating things to `check`

--- test/microcosmos/io_test.clj
+++ test/microcosmos/io_test.clj
@@ -1,21 +1,25 @@
 (ns microcosmos.io-test
   (:require [midje.sweet :refer :all]
-            [microcosmos.io :as io]))
+            [microcosmos.io :as io]
+            [clojure.test :refer :all]
+            [check.core :refer :all]))
 
-(facts "about serializing JSON"
-  (fact "serializes in a clojure way"
-    (io/serialize-msg {:foo-one 10}) => "{\"foo_one\":10}"
-    (io/serialize-msg {:foo/one 10}) => "{\"one\":10}")
+(deftest json-serialize
+  (testing "serializes in a clojure way"
+    (check (io/serialize-msg {:foo-one 10}) => "{\"foo_one\":10}")
+    (check (io/serialize-msg {:foo/one 10}) => "{\"one\":10}"))
 
-  (fact "demunges function names"
-    (io/serialize-msg string?) => #"clojure.core/string\?")
+  (testing "demunges function names"
+    (check (io/serialize-msg string?) => #"clojure.core/string\?"))
 
-  (fact "demunges atoms or references"
-    (io/serialize-msg (atom "foo")) => #"foo")
+  (testing "demunges atoms or references"
+    (check (io/serialize-msg (atom "foo")) => #"foo"))
 
-  (fact "serializes exception info"
-    (let [ex (try (throw (ex-info "FOO" {:foo integer? :bar (atom nil)})) (catch Throwable e e))]
-      (io/serialize-msg ex)) => #"FOO"))
+  (testing "serializes exception info"
+    (let [ex (try (throw (ex-info "FOO" {:foo integer? :bar (atom nil)}))
+               (catch Throwable e e))]
+      (check (io/serialize-msg ex) => #"FOO"))))
 
-(fact "de-serializes JSON in a clojure way"
-  (io/deserialize-msg "{\"foo_one\":10}") => {:foo-one 10})
+(deftest json-deserialize
+  (testing "de-serializes JSON in a clojure way"
+    (check (io/deserialize-msg "{\"foo_one\":10}") => {:foo-one 10})))
--- test/microcosmos/io_test.cljs
+++ test/microcosmos/io_test.cljs
@@ -1,18 +1,15 @@
 (ns microcosmos.io-test
-  (:require [clojure.test :refer-macros [deftest is testing run-tests async]]
+  (:require [clojure.test :refer-macros [deftest testing]]
+            [check.core :refer-macros [check]]
             [microcosmos.io :as io]))
 
 (deftest serializing-json
   (testing "serializing in a clojure way"
-    (is (= "{\"foo_one\":10}"
-           (io/serialize-msg {:foo-one 10})))
-
-    (is (= "{\"one\":10}"
-          (io/serialize-msg {:foo/one 10}))))
+    (check (io/serialize-msg {:foo-one 10}) => "{\"foo_one\":10}")
+    (check (io/serialize-msg {:foo/one 10}) => "{\"one\":10}"))
 
   (testing "demunges atoms or references"
-    (is (= "\"#atom foo\""
-           (io/serialize-msg (atom "foo"))))))
+    (check (io/serialize-msg (atom "foo")) => "\"#atom foo\"")))
 
   ; (testing "serializes exception info"
   ;   (let [ex (try (throw (ex-info "FOO" {:foo integer? :bar (atom nil)})) (catch js/Error e e))]
@@ -21,5 +18,4 @@
 
 (deftest deserializing-json
   (testing "de-serializes JSON in a clojure way"
-    (is (= {:foo-one 10}
-           (io/deserialize-msg "{\"foo_one\":10}")))))
+    (check (io/deserialize-msg "{\"foo_one\":10}") => {:foo-one 10})))
