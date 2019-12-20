55c2048e5c17df487d79795bdcfeec6024cbab02
Maurício Szabo <mauricio.szabo@gmail.com>
2019-12-20 17:44:56 -0300

    Bump midje

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/acessocard/microscope"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [cheshire "5.6.1"]
                  [finagle-clojure/core "0.7.0"]
                  [finagle-clojure/http "0.7.0"]
                  [environ "1.1.0"]]
 
   :profiles {:dev {:src-paths ["dev"]
-                   :dependencies [[midje "1.8.3"]]
+                   :dependencies [[midje "1.9.9"]]
                    :plugins [[lein-midje "3.2.1"]]}})
